3cabdfc62b414a788354c4601bf12054b24ecc2a
Box-Cat <pjh211pjh@gmail.com>
2023-02-23 23:40:04 +0900

    230223

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -3,11 +3,12 @@ import { useEffect } from 'react'
 import { GetCurrentUser } from '../apicalls/users'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-hot-toast'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { HideLoader, ShowLoader } from '../redux/loaderSlice'
+import { SetUser } from '../redux/userSlice'
 
 const ProtectedRoute = ({ children }) => {
-  const [user, setUser] = React.useState(null);
+  const {user} = useSelector(state => state.userReducer)
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const getCurrentUser = async () => {
@@ -16,7 +17,7 @@ const ProtectedRoute = ({ children }) => {
       const response = await GetCurrentUser();
       dispatch(HideLoader())
       if (response.success) {
-        setUser(response.data);    
+        dispatch(SetUser(response.data));
       } else {
         dispatch(HideLoader())
         toast.error(response.message);
@@ -35,10 +36,20 @@ const ProtectedRoute = ({ children }) => {
   }, [])
 
   return (
-    <div> 
-      <h1>{user?.name}</h1>
-      <h1>{user?.email}</h1>
-      {children}
+    <div className='h-screen w-screen bg-gray-100 p-2'> 
+      
+      <div className='flex justify-between p-5'>
+        <div className='flex items=center gap-1'>
+            <i className="ri-message-3-line text-2xl"></i>
+            <h1 className="text primary text-2xl uppercase font-bold">Chat</h1>
+        </div>
+        <div className='flex gap-1 text-ml'>
+            <i class="ri-shield-user-line"></i>
+            <h1 className='underline'>{user?.name}</h1>
+        </div>
+      </div>
+
+      <div className='p-5'>{children}</div>
     </div>
   )
 }
